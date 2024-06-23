f0b0ff5c838446abfdad9393e27ef0fb3e938f9b
disc-loop <tom@recursivediscourse.com>
2024-06-23 20:39:32 +1000

    Define basic structure

--- arithmetic.c
+++ arithmetic.c
@@ -2,17 +2,35 @@
 /* #include <stdlib.h> */
 /* #include <time.h> */
 
-// This program is designed to quiz the user on arithmetic. It does so by asking them to evaluate randomly generated expressions.
+// This program is designed to quiz the user on arithmetic. It does so by asking them to evaluate randomly generated expressions. Inspired by [Stack Overflow](https://stackoverflow.com/questions/14472680/how-to-get-and-evaluate-the-expressions-from-a-string-in-c) post my Steve Summit
 // TODO: Generate an expression
-// TODO: Generate solution for a given expression
+char *genExpr(void);
+// TODO: Evaluate solution for a given expression
+char *eval(char *expr);
 // TODO: Prompt the user with the expression
-// TODO: Evaluate expression
+// TODO: Evaluate user's solution
 // TODO: Give user feedback on their solution, e.g. right or wrong, time taken to evaluate, etc.
-int main () {
+
+int main (void) {
+  char *expr = genExpr();
+  char *solution = eval(expr);
   char line[10];
   while (1) {
-    printf("Say something\n");
+    printf("Solve: %s\n", expr);
     fgets(line, sizeof line, stdin);
-    printf("%s", line);
+    if (*line == *solution) {
+      printf("Correct!\n");
+    } else {
+      printf("Incorrect\n");
+    }
   }
+}
+
+char *genExpr(void) {
+  return "2 + 2";
+}
+
+char *eval(char *expr) {
+  if (expr) {};
+  return "4";
 }
